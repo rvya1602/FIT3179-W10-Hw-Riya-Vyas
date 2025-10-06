28501fb31d010d5cc7255f7b190387a08f39a6e8
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-07 00:01:14 +1100

    Barchart complete.

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "js/barchart.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#barchart", vg_1, { actions : false }).then(function(result) {
+}).catch(console.error);
+
+
