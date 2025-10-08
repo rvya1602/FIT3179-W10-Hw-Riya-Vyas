aafe2ce462ef91d66367b375e883e50d5562a109
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-08 23:10:54 +1100

    Barchart for international students by top 20 countries of origin.

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,4 +3,7 @@ var vg_1 = "js/barchart.vg.json";
 vegaEmbed("#barchart", vg_1, { actions : false }).then(function(result) {
 }).catch(console.error);
 
-
+var vg_2 = "js/int_students_by_cob_2024.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#int_students_by_cob", vg_2, { actions : false }).then(function(result) {
+}).catch(console.error);
