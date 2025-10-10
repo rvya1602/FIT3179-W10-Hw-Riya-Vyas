f7732feaf235705bc7e37649aa4d234e000bc16c
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-10 17:53:51 +1100

    Minor update.

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -7,3 +7,8 @@ var vg_2 = "js/int_students_by_cob_2024.vg.json";
 // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
 vegaEmbed("#int_students_by_cob", vg_2, { actions : false }).then(function(result) {
 }).catch(console.error);
+
+var vg_3 = "js/bach_or_higher_by_state.vg.json";
+// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+vegaEmbed("#pop_degree_by_state", vg_3, { actions : false }).then(function(result) {
+}).catch(console.error);
