137dd1419a6cfc8448eb4abe9300f09bf7f51e38
rvya1602 <126407677+rvya1602@users.noreply.github.com>
2025-10-10 18:40:45 +1100

    -

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,7 +1,7 @@
-var vg_1 = "js/barchart.vg.json";
-// { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
-vegaEmbed("#barchart", vg_1, { actions : false }).then(function(result) {
-}).catch(console.error);
+// var vg_1 = "js/barchart.vg.json";
+// // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
+// vegaEmbed("#barchart", vg_1, { actions : false }).then(function(result) {
+// }).catch(console.error);
 
 var vg_2 = "js/int_students_by_cob_2024.vg.json";
 // { actions : false } to hide the "Open in Vega" etc... actions (the three dots that show)
